23b2471f695d16e323762d9cc00b62d584969aea
i354422 <georgi.bozhinov@sap.com>
2018-08-20 10:20:46 +0300

    Refactor some `var` identifiers

--- test.js
+++ test.js
@@ -50,12 +50,12 @@ function writePackageJson (version, option) {
 
 function writeVersionPy (version) {
   shell.exec('rm package.json')
-  var versionString = '# This is a comment \n __version__ = ' + version
+  const versionString = '# This is a comment \n __version__ = ' + version
   fs.writeFileSync('__version__.py', versionString, 'utf-8')
 }
 
 function writeCustomVersionPy (version) {
-  var versionString = '# This is a comment \n __version__ = ' + version
+  const versionString = '# This is a comment \n __version__ = ' + version
   fs.writeFileSync('VERSION', versionString, 'utf-8')
 }
 
